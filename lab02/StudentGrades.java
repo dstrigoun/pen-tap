00bdc9162e4d2d5c2c97c6f0c1dd4feb8003a222
dstrigoun <dstrigoun@outlook.com>
2017-04-16 11:09:14 -0700

    added lab02 src code

---
+++ lab02/StudentGrades.java
@@ -0,0 +1,32 @@
+package p1;
+
+/**
+ * StudentGrades.
+ * 
+ * @author A00983479
+ * @version 21/01/17
+ */
+public class StudentGrades {
+	/**
+	 * Drives the program.
+	 * @param args command line args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("///////////////////" + 
+						   "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+		System.out.println("==	    Student Points" + 
+						   "	    ==");
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" + 
+						   "///////////////////");
+		System.out.println(" ");
+		System.out.println("Name \t Lab \t Bonus \t Total");
+		System.out.println("---- \t --- \t ----- \t -----");
+		System.out.println("John \t 35 \t 10 \t " + (35+10));
+		System.out.println("Sally \t 49 \t 4 \t " + (49+4));
+		System.out.println("Stacey \t 90 \t 10 \t " + (90+10));
+		System.out.println("Maria \t 20 \t 1.5 \t " + (20+1.5));
+		System.out.println("Ksenia \t 89 \t 9 \t " + (89+9));
+	}
+
+}
